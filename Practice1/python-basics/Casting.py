4ec13eada675468d4ce138b2df6a80a9a310af2c
zulharnai08-alt <zulharnai08@gmail.com>
2026-01-28 21:31:47 +0600

    Update Casting.py

---
+++ Practice1/python-basics/Casting.py
@@ -0,0 +1,18 @@
+# Casting (type conversion)
+
+# int to float
+x = float(10)
+print(x)        # 10.0
+
+# float to int
+y = int(5.9)
+print(y)        # 5
+
+# string to int
+z = int("20")
+print(z)        # 20
+
+# int to string
+a = str(100)
+print(a)
+print(type(a))  # <class 'str'>
