b2aacb40015a4a2d89c8db9c6a8e9f092a500552
zulharnai08-alt <zulharnai08@gmail.com>
2026-03-20 11:15:12 +0500

    Update write_files.py

---
+++ Practice6/file_handling/write_files.py
@@ -0,0 +1,13 @@
+# Read file (A1 level)
+
+# open file and write all text
+file = open("example.txt", "w")
+
+# read content
+text = file.read()
+
+# print content
+print(text)
+
+# close file
+file.close()
