37347c403a573a93e18e02a4c1bdc63bac4afc7c
zulharnai08-alt <zulharnai08@gmail.com>
2026-03-20 11:16:52 +0500

    Update map_filter_reduce.py

--- Practice6/builtin_functions/map_filter_reduce.py
+++ Practice6/builtin_functions/map_filter_reduce.py
@@ -1,1 +1,17 @@
 
+from functools import reduce
+
+# list of numbers
+numbers = [1, 2, 3, 4, 5]
+
+# map: make square numbers
+squares = list(map(lambda x: x * x, numbers))
+print("Squares:", squares)
+
+# filter: take only even numbers
+even = list(filter(lambda x: x % 2 == 0, numbers))
+print("Even numbers:", even)
+
+# reduce: sum all numbers
+total = reduce(lambda x, y: x + y, numbers)
+print("Sum:", total)
